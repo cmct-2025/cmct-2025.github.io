a1d4188a0adc433b55e01021802932e332801cfa
PoChen <icerkure@gmail.com>
2025-03-25 20:57:56 +0800

    chore: update venue

--- src/views/VenueView.vue
+++ src/views/VenueView.vue
@@ -6,8 +6,13 @@ import Content from '../layout/Content.vue';
 <template>
   <Content :align="'left'">
     <div class="context">
-      <h2>TBD...</h2>
-      
+      <!-- <h2>TBD...</h2> -->
+      <section>
+        <p>先鋒國際研發大樓 3樓 會議廳</p>
+        <p>Pioneer International R&D Building, PB</p>
+        <img src="https://academic.ntut.edu.tw/media/f1ldkj5z/campusmap_eng.jpg" style="max-width: 100%;"/>
+      </section>
+
     </div>
   </Content>
 </template>
@@ -17,5 +22,6 @@ import Content from '../layout/Content.vue';
   height: auto;
   min-height: 40vh;
   padding: 2rem 3rem;
+  padding-bottom: 16rem;
 }
 </style>
